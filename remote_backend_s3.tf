a598c22754348f279dd3069b69433e28637158f8
Arbaz Mallabadkar <arbmalla@publicisgroupe.net>
2024-07-14 13:03:49 +0530

    updated stuff for vpc s3 creation

--- remote_backend_s3.tf
+++ remote_backend_s3.tf
@@ -1,7 +1,8 @@
-terraform {
+/*terraform {
   backend "s3" {
-    bucket = "dev-proj-1-jenkins-remote-state-bucket-123456"
+    bucket = "psdev-proj-1-jenkins-remote-state-bucket-123456"
     key    = "devops-project-1/jenkins/terraform.tfstate"
-    region = "eu-west-1"
+    region = "us-east-1"
   }
 }
+*/
