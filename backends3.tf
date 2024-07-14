2a786744c017bd901030ce413e8ca7f466dc45fc
Arbaz Mallabadkar <arbmalla@publicisgroupe.net>
2024-07-14 17:35:42 +0530

    updated

---
+++ backends3.tf
@@ -0,0 +1,25 @@
+resource "aws_s3_bucket" "mybebucket" {
+    bucket = "s3backendaz41300"
+    versioning {
+      enabled = true
+    }
+    server_side_encryption_configuration {
+      rule {
+        apply_server_side_encryption_by_default {
+          sse_algorithm = "AES256"
+        }
+      }
+    }
+}
+resource "aws_dynamodb_table" "staelockaz" {
+    name = "state-lock"
+    billing_mode = "PAY_PER_REQUEST"
+    hash_key = "LockID"
+
+    attribute {
+      name = "LockID"
+      type = "S"
+
+    }
+  
+}
--- remote_backend_s3.tf
+++ remote_backend_s3.tf
@@ -1,8 +1,15 @@
-/*terraform {
+/*
+terraform {
   backend "s3" {
-    bucket = "psdev-proj-1-jenkins-remote-state-bucket-123456"
-    key    = "devops-project-1/jenkins/terraform.tfstate"
+    bucket = aws_s3_bucket.my_bucket.bucket
+    key    = "terraform-tfstate"
     region = "us-east-1"
   }
 }
+resource "aws_s3_bucket" "my_bucket" {
+  bucket = "dev-proj-1-jenkins-remote-state"
+  acl    = "public"
+   
+  depends_on = [ aws_s3_bucket.my_bucket ]
+  }
 */
